cf8c644d903c689c5cb6aa9351469a0ff26ec068
evenage <charue.vyago2019@gmail.com>
2025-01-21 08:21:43 +0200

    changes

---
+++ server.js
@@ -0,0 +1,48 @@
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
+
+//index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+
+})
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
